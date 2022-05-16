3b6346587405de425a62aa28ea8cb6caa6e29c8d
manojkumar@877 <kbmanojkumar.20cs@srit.org>
2022-05-16 22:48:21 +0200

    tabs added

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script>
-import Header from "./components/Header.svelte";
-import Footer from "./components/Footer.svelte";
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
 </script>
-<main>
 
-<Header/>
-<Footer/>
-  </main>
+<Header />
+
+<main>
+  <Tabs on:tabChange={tabChange} {activeItem} {items} />
+  {#if activeItem === "Current Polls"}
+    <p>Hei poll</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>new Poll</p>
+  {/if}
+</main>
+<Footer />
